b96b504f3242fe7a005d0d078b0310e94499dc65
kikutestground <holdingkishlay@gmail.com>
2024-07-09 20:09:30 +0530

    Finalising

--- scripts/screen.js
+++ scripts/screen.js
@@ -1,17 +1,17 @@
-function detectDevice() {
-    if (window.innerWidth < 1450) {
-        $('header').css('display', 'none');
-        $('main').css('display', 'none');
-        $('footer').css('display', 'none');
-        $('center').css('display', 'block');
-    }
-    else {
-        $('center').css('display', 'none');
-        $('header').css('display', '');
-        $('main').css('display', '');
-        $('footer').css('display', '');
-    }
-}
+// function detectDevice() {
+//     if (window.innerWidth < 1450) {
+//         $('header').css('display', 'none');
+//         $('main').css('display', 'none');
+//         $('footer').css('display', 'none');
+//         $('center').css('display', 'block');
+//     }
+//     else {
+//         $('center').css('display', 'none');
+//         $('header').css('display', '');
+//         $('main').css('display', '');
+//         $('footer').css('display', '');
+//     }
+// }
 
-window.onload = detectDevice;
-window.onresize = detectDevice;
+// window.onload = detectDevice;
+// window.onresize = detectDevice;
